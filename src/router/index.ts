677d1af9ee7149cfd1e507b8d2a8d31e5803cedf
谢小龙 <742875110@qq.com>
2023-02-02 11:20:05 +0800

    首页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import WelcomePage from "@/views/WelcomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'welcomePage',
+      component: WelcomePage
+    },
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <p class="font_container">Welcome To</p>
+      <p class="font_container">The FrontEnd</p>
+      <p class="font_container">ApplicationFramework</p>
+      <div class="btn_container" @click="selectContainer()">
+        Start
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    selectContainer(val) {
+      window.sessionStorage.setItem('selectType', val)
+      this.$router.push('/Home')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.login_container {
+  height: 100%;
+  background-image: url("../assets/loginImg/featherFrontCover.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+.login_box {
+  width: 580px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.font_container {
+  text-align: center;
+  font-family: Lora, serif;
+  letter-spacing: 0.2em;
+  color: white;
+  font-size: 250%;
+  text-shadow: 0 2px 2px black;
+}
+
+.btn_container {
+  width: 150px;
+  cursor: pointer;
+  border: 1px white solid;
+  border-radius: 25px;
+  position: absolute;
+  left: 50%;
+  top: 130%;
+  transform: translate(-50%, -50%);
+
+  font-family: Lora, serif;
+  color: white;
+  text-align: center;
+  font-size: 200%;
+  text-shadow: 0 2px 2px black;
+}
+
+</style>
